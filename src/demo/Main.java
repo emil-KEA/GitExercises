b50bcd7cc2a10e4da0f33c95e6ebc0d5573cbbcf
emil-KEA <emil33v4@stud.kea.dk>
2022-02-25 09:27:34 +0100

    added askAboutAge

--- src/demo/Main.java
+++ src/demo/Main.java
@@ -6,12 +6,21 @@ public class Main {
     Scanner in = new Scanner(System.in);
 
     public static void main(String[] args) {
-        new Main().greeting();
+        new Main().go();
         System.out.println("It works!");
+    }
+    void go(){
+        greeting();
+        askAboutAge();
+        System.out.println(askAboutAge());
     }
     void greeting(){
         System.out.println("Enter your name: ");
         String name = in.nextLine();
         System.out.println("Hej " + name);
+    }
+    int askAboutAge(){
+        System.out.println("Enter your age");
+        return in.nextInt();
     }
 }
