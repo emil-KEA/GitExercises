62374ac4d61697bab565c08793a240419aa972aa
emil-KEA <emil33v4@stud.kea.dk>
2022-02-25 09:20:41 +0100

    added greet

--- src/demo/Main.java
+++ src/demo/Main.java
@@ -1,8 +1,17 @@
 package demo;
 
+import java.util.Scanner;
+
 public class Main {
+    Scanner in = new Scanner(System.in);
 
     public static void main(String[] args) {
-        System.out.println("Halå!");
+        new Main().greeting();
+        System.out.println("It works!");
+    }
+    void greeting(){
+        System.out.println("Enter your name: ");
+        String name = in.nextLine();
+        System.out.println("Hej " + name);
     }
 }
